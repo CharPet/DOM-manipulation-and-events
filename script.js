73554686ca12fa7eeca72fcbefa47184485cba29
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-23 13:55:45 +0300

    Added div and the elements inside it

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ h3.style.cssText = 'color: blue;';
 h3.textContent = "I'm a blue h3!";
 
 container.appendChild(h3);
+
+const div = document.createElement('div');
+div.style.cssText = 'border: 2px solid black; background-color: pink;';
+    const h1 = document.createElement('h1');
+    h1.textContent = "I'm in a div";
+    div.appendChild(h1);
+
+    const pInDiv = document.createElement('p');
+    pInDiv.textContent = "ME TOO!";
+    div.appendChild(pInDiv);
+
+container.appendChild(div);
+
