b8d8ec6374275d08bd8698ad36220e45d51e2a39
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-07-23 13:41:20 +0300

    Added <h3> element

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ p.style.cssText = 'color: red;';
 p.textContent = "Hey I'm red!";
 
 container.appendChild(p);
+
+
+const h3 = document.createElement('h3');
+h3.style.cssText = 'color: blue;';
+h3.textContent = "I'm a blue h3!";
+
+container.appendChild(h3);
